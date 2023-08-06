07cbc25b375db58781d497f13f0296000332d0dc
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-08-06 11:26:57 +0200

    Update deploy_dapp.php

--- deploy_dapp.php
+++ deploy_dapp.php
@@ -58,3 +58,7 @@ if (empty($dapp)) exit;
 try {
 	$query = $db->prepare('UPDATE `dapps` SET `deployed` = 1, `address`=:address WHERE `user` = :user AND `id` = :id');
 	$query->bindParam(':id', $id);
+	$query->bindParam(':user', $user);
+	$query->bindParam(':address', $address);
+	$query->execute();
+} catch (PDOException $e) {
