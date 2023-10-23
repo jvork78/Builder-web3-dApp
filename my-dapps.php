113a02b1dc29b1e343def78e23a90115d78c4f0b
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-10-23 13:38:11 +0200

    Update my-dapps.php

--- my-dapps.php
+++ my-dapps.php
@@ -118,3 +118,6 @@ require_once('common/header.php');
                             <div class="input-group colorpicker-component colorpicker-input">
                             	<input name="text_color" type="text" class="form-control input-lg" value="<?php echo $interface['text_color']; ?>" required>
                             	<span class="input-group-addon"><i></i></span>
+                            </div>
+						</div>						
+						<div class="one-settings">
