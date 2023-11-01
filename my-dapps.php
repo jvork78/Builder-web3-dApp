1cfd968f16ca8e9fbfe6cc7919efe12693a1521e
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-11-01 16:21:40 +0100

    Update my-dapps.php

--- my-dapps.php
+++ my-dapps.php
@@ -135,3 +135,5 @@ require_once('common/header.php');
                             </div>
 						</div>
 						<div class="one-settings">
+							<p class="text-center"><?php echo $_mydapps['ethereum_add_color']; ?></p>
+							<div class="input-group colorpicker-component colorpicker-input">
