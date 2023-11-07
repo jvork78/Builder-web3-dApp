70b49dd81a0bea16ea7de00c568191a5b13dfa55
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-11-07 11:34:10 +0100

    Update my-dapps.php

--- my-dapps.php
+++ my-dapps.php
@@ -150,3 +150,7 @@ require_once('common/header.php');
 						</div>
 						<div class="one-settings">
                             <p class="text-center"><?php echo $_mydapps['cancel_btn_color']; ?></p>
+                            <div class="input-group colorpicker-component colorpicker-input">
+                                <input name="cancel_buttons_color" type="text" class="form-control input-lg" value="<?php echo $interface['cancel_buttons_color']; ?>" required>
+                                <span class="input-group-addon"><i></i></span>
+							</div>
