3353f3b76dba994c0aa9578653e800ea18993daa
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-11-13 11:38:40 +0100

    Update my-dapps.php

--- my-dapps.php
+++ my-dapps.php
@@ -169,3 +169,5 @@ require_once('common/header.php');
 			<div class="modal-content">
 				<div class="modal-header text-center">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h2><?php echo $_mydapps['template_settings']; ?></h2>
+				</div>
