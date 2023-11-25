0a6db3eb40e6ed9278f8abb530753fbef2041399
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-11-25 13:19:33 +0100

    Update my-dapps.php

--- my-dapps.php
+++ my-dapps.php
@@ -196,3 +196,6 @@ require_once('common/header.php');
 						</div>
 						<div class="one-settings">
                             <p class="text-center"><?php echo $_mydapps['links_color']; ?></p>
+                            <div class="input-group colorpicker-component colorpicker-input">
+                                <input name="links_color" type="text" class="form-control input-lg" value="<?php echo $interface['links_color']; ?>" required>
+                                <span class="input-group-addon"><i></i></span>
