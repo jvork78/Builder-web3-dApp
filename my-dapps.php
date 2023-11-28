ddf01ad209e64f4e2f41be467bd11d5d42481321
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-11-28 11:51:23 +0100

    Update my-dapps.php

--- my-dapps.php
+++ my-dapps.php
@@ -204,3 +204,5 @@ require_once('common/header.php');
 						<div class="one-settings">
 							<p class="text-center"><?php echo $_mydapps['ethereum_add_color']; ?></p>
 							<div class="input-group colorpicker-component colorpicker-input">
+								<input name="eth_addresses_color" type="text" class="form-control input-lg" value="<?php echo $interface['eth_addresses_color']; ?>" required>
+								<span class="input-group-addon"><i></i></span>
