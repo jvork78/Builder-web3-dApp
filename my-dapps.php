65961a6df4d6af0a1c686cd8be92d67f81162eae
jvork78 <116947668+jvork78@users.noreply.github.com>
2023-12-09 10:04:43 +0100

    Update my-dapps.php

--- my-dapps.php
+++ my-dapps.php
@@ -220,3 +220,5 @@ require_once('common/header.php');
 <?php } elseif ($dapp->getDappType() == 'multisig') { ?>
 	<div id="templateModal<?php echo $dapp->getId(); ?>" class="modal fade" role="dialog" aria-hidden="true">
 		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header text-center">
